aee7139cf5fa713697eedf70683b6219218e46c4
alexandre2297 <alexandre28200@gmail.com>
2018-10-17 17:05:00 +0200

    correction creation table

--- data/db.sql
+++ data/db.sql
@@ -7,30 +7,31 @@ CREATE TABLE "User" (
     ects integer NOT NULL
 );
 
-CREATE TABLE "Game" (
+CREATE TABLE Game (
    id SERIAL PRIMARY KEY,
-   id_j1 integer REFERENCES User(id),
-   id_j2 integer REFERENCES User(id),
+   id_j1 integer REFERENCES "User"(id),
+   id_j2 integer REFERENCES "User"(id),
    status VARCHAR NOT NULL,
    cartes VARCHAR NOT NULL,
    message VARCHAR NOT NULL,
-   id_winner integer REFERENCES User(id),
+   id_winner integer REFERENCES "User"(id),
    po integer NOT NULL
 );
 
-Create table category (
+Create table Category (
 	id serial primary key,
-	attack integer,
-	defence integer,
-	type varchar,
-	cost integer,
-	chance float
+	attack integer NOT NULL,
+	defence integer NOT NULL,
+	type varchar NOT NULL,
+	cost integer NOT NULL,
+	chance float NOT NULL
 );
 Create table Unit(
-	name varchar,
+	id integer primary key,
+	name varchar NOT NULL,
 	id_cat integer references category(id),
-	AtckBonus integer,
-	DefBonus integer,
-	chanceBonus float,
-	description varchar
+	AtckBonus integer default 0,
+	DefBonus integer default 0,
+	chanceBonus float default 0,
+	description varchar NOT NULL
 );
