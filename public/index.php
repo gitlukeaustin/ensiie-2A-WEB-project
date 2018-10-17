346e8cfbdeedabeed69c5be68f07ff9593c4210b
Luke Austin <luke.austin@ensiie.fr>
2018-10-17 22:17:02 +0200

    entity/repo Unit

--- public/index.php
+++ public/index.php
@@ -7,8 +7,12 @@ $dbUser = getenv('DB_USER');
 $dbPassword = getenv('DB_PASSWORD');
 $connection = new PDO("pgsql:host=postgres user=$dbUser dbname=$dbName password=$dbPassword");
 
-$userRepository = new \User\UserRepository($connection);
-$users = $userRepository->fetchAll();
+//$userRepository = new \User\UserRepository($connection);
+//$users = $userRepository->fetchAll();
+
+$unitRepository = new \Unit\UnitRepository($connection);
+$units = $unitRepository->fetchAll();
+var_dump($units);
 ?>
 
 <html>
@@ -21,23 +25,8 @@ $users = $userRepository->fetchAll();
 <div class="container">
     <h3><?php echo 'Docker! php' . PHP_VERSION; ?></h3>
 
-    <table class="table table-bordered table-hover table-striped">
-        <thead style="font-weight: bold">
-            <td>#</td>
-            <td>Firstname</td>
-            <td>Lastname</td>
-            <td>Age</td>
-        </thead>
-        <?php /** @var \User\User $user */
-        foreach ($users as $user) : ?>
-            <tr>
-                <td><?php echo $user->getId() ?></td>
-                <td><?php echo $user->getFirstname() ?></td>
-                <td><?php echo $user->getLastname() ?></td>
-                <td><?php echo $user->getAge() ?> years</td>
-            </tr>
-        <?php endforeach; ?>
-    </table>
+   
+   
 </div>
 </body>
 </html>
