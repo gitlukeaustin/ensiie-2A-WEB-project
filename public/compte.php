f18969c1641a5199363ea6981dfd5517c1e8f417
omartalibi <omar.talibi.96@gmail.com>
2018-10-28 01:22:30 +0200

    add some modifs

--- public/compte.php
+++ public/compte.php
@@ -2,7 +2,6 @@
 require '../vendor/autoload.php';
 use User\UserRepository;
 
-
 $dbName = getenv('DB_NAME');
 $dbUser = getenv('DB_USER');
 $dbPassword = getenv('DB_PASSWORD');
@@ -19,10 +18,29 @@ if(isset($_SESSION['login']) && $_SESSION['login'] != null && isset($_SESSION['u
 }
 
 
+
+
 ?>
+<script type="text/javascript">
+	var columns = ["login", "email", "ects"];
+
+	window.onload = function () {
+		for(index in columns)
+			document.getElementById(columns[index]).disabled = false;
+	};
+
+	function enableInputs(login, email, ects){
+		login.disabled = true;
+		email.disabled = false;
+		ects.disabled = false;
+	}
+</script>
+
+
 
 <html>
-<link href="../css/login.css" rel="stylesheet"></link>
+<link href="../css/compte.css" rel="stylesheet"></link>
+
 <div class="wrapper fadeInDown">
     <div id="formContent">
         <!-- Tabs Titles -->
@@ -37,22 +55,31 @@ if(isset($_SESSION['login']) && $_SESSION['login'] != null && isset($_SESSION['u
         <br /><br />
 
         <?php if(isset($_SESSION['login']) && $_SESSION['login'] != null) { ?>
-	            <b>Login : <input type="text" id="login" class="fadeIn second" style="width: 200px;" name="login" value="<? echo $user->getLogin() ?>" placeholder="username" disabled="true" />
+	            <b>Login : <input type="text" id="login" class="fadeIn second" style="width: 200px;" name="login" value="<? echo $user->getLogin() ?>" placeholder="username" disabled />
 	            <br />
-	            Email : <input type="text" id="email" class="fadeIn third" style="width: 200px;" name="email"  value="<? echo $user->getEmail() ?>" placeholder="email" disabled="true">
+	            Email : <input type="text" id="email" class="fadeIn third" style="width: 200px;" name="email"  value="<? echo $user->getEmail() ?>" placeholder="email" disabled />
 	            <br />
-	            Ects : &nbsp; <input type="text" id="etcs" class="fadeIn third" style="width: 200px;" name="ects"  value="<? echo $user->getEcts() ?>" placeholder="ects" disabled="true">
+	            Ects : &nbsp; <input type="text" id="ects" class="fadeIn third" style="width: 200px;" name="ects"  value="<? echo $user->getEcts() ?>" placeholder="ects" disabled />
 	        	</b>
-	        	<form method="POST" action="delete.php">  
-	        		<input type="hidden" name='user_id' value='<? echo $user->getId() ?>' >
-	        		<input type="submit" class="fadeIn fourth" value="delete" style="width: 80px;" >
-	        	</form>
+	        	<br /><br /><br />
+	        	<table>
+	        		<tr>
+	        			<td>
+	        				<form method="POST" action="delete.php">  
+				        		<input type="hidden" name='user_id' value='<? echo $user->getId() ?>' >
+				        		<input type="submit" class="fadeIn fourth" value="delete" style="width: 100px;text-align: center" >
+				        	</form>
+	        			</td>
+	        			<td>
+	        				<form method="POST" action="compte.php">
+				        		<input type="hidden" name='_user_id' value='<? echo $user->getId() ?>' >
+					        	<input type="submit" class="fadeIn fourth" value="modify" style="width: 100px;text-align: center" >
+				        	</form>
+	        			</td>
+	        		</tr>
+	        	</table>
 
-	        	<form method="POST" action="modify.php">
-		        	<input type="submit" class="fadeIn fourth" value="modify" >
-	        	</form>
         <?php } ?>
-
 
         <!-- Remind Passowrd -->
         <div id="formFooter">
@@ -61,5 +88,17 @@ if(isset($_SESSION['login']) && $_SESSION['login'] != null && isset($_SESSION['u
 
     </div>
 </div>
-
 </html>
+
+<?php
+
+if(isset($_POST['_user_id']))
+{
+   modify();
+} 
+
+function modify(){
+    echo "modify " . $_POST["_user_id"]. '<script type="text/javascript"> enableInputs(document.getElementById("login"), document.getElementById("email"), document.getElementById("ects")); </script>';
+}
+
+?>
