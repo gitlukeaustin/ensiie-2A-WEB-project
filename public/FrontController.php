e77be560551b9676324049862b6cdc8fafcca77a
Luke Austin <luke.austin@ensiie.fr>
2018-11-06 09:39:01 +0100

    refacturing du frontcontroller

--- public/FrontController.php
+++ public/FrontController.php
@@ -8,6 +8,13 @@ class FrontController
     private $params;
     private $url;
     private $connection;
+    private $unitRepository;
+    private $unitHydro;
+    private $catHydro;
+    private $catRepository;
+    private $sim;
+    private $gameRepository;
+    private $gameHydrator;
 
     public function __construct($connection)
     {
@@ -17,6 +24,13 @@ class FrontController
             'get' => $_GET,
             'post' => $_POST
         ];
+        $this->unitRepository = new \Unit\UnitRepository($this->connection);
+        $this->unitHydro = new \Unit\UnitHydrator();
+        $this->catRepository = new \Category\CategoryRepository($this->connection);
+        $this->catHydro = new \Category\CategoryHydrator();
+        $this->sim = new Simulator();
+        $this->gameRepository = new \Game\GameRepository($this->connection);
+        $this->gameHydrator = new \Game\GameHydrator();
     }
 
     public function dispatch()
@@ -30,35 +44,25 @@ class FrontController
             //include(__DIR__.'../../../view/' . $this->url[1] . '/' . $this->url[2]);
             //return $controller->$actionName();
 
-        
-
             if (isset($this->params['get']['action'])) {
                 if ($this->params['get']['action'] == 'fetch_units') {
-                    $unitRepository = new \Unit\UnitRepository($this->connection);
-                    $unitHydro = new \Unit\UnitHydrator();
-                    $units = $unitRepository->fetchAll();
+                    $units = $this->unitRepository->fetchAll();
                     $unitArray = [];
                     foreach ($units as $unit) {
-                        $unitArray[] = $unitHydro->extract($unit);
+                        $unitArray[] = $this->unitHydro->extract($unit);
                     }
-                    //var_dump($unitArray);
                     echo json_encode($unitArray);
                     return true;
-                } else if ($this->params['get']['action'] == 'fetch_categories') {
-                    $catRepository = new \Category\CategoryRepository($this->connection);
-                    $catHydro = new \Category\CategoryHydrator();
-                    $cats = $catRepository->fetchAll();
+                } else if ($this->params['get']['action'] == 'fetch_categories') { 
+                    $cats = $this->catRepository->fetchAll();
                     $catArray = [];
                     foreach ($cats as $cat) {
-                        $catArray[] = $catHydro->extract($cat);
+                        $catArray[] = $this->catHydro->extract($cat);
                     }
-                    //var_dump($catArray);
-                    $simplearray = ["a", "b", "c"];
                     echo json_encode($catArray);
                     return true;
                 } 
                 else if ($this->params['get']['action'] == 'simulate') {
-                    
                     $data = json_decode($this->params['post']['data'],true); // decode as array
                     $j1 = $_SESSION['user']; // $j1 = userHydro->hydrate($_SESSION['user'])
                     
@@ -68,11 +72,10 @@ class FrontController
                     $cartesRobot = [['type'=>'Soldat','name'=>'Soldat','attack'=>2,'defence'=>2,'chance'=>0.9],
                         ['type'=>'Mur','name'=>'Mur','attack'=>0,'defence'=>2,'chance'=>0.9],
                         ['type'=>'Soldat','name'=>'Soldat','attack'=>2,'defence'=>2,'chance'=>0.9]];
-                    
-                    $sim = new Simulator();
-                    $winner = $sim->simulate($data['selected'],$cartesRobot,$j1,$robot);
+        
+                    $winner = $this->sim->simulate($data['selected'],$cartesRobot,$j1,$robot);
                     //array_push($log,$sim->getLog()??['']);
-                    foreach($sim->getLog()??[''] as $l){
+                    foreach($this->sim->getLog()??[''] as $l){
                         $log[] = $l;
                     }
                     
@@ -83,32 +86,29 @@ class FrontController
                         $log[] = "Le joueur ".$winner['login']." a gagné!";
                     }
                     
-                    echo json_encode(['data' => $data['selected'], 'animations' => $sim->getAnimations() ,'log' => $log, 'adv_cards' => $cartesRobot, 'adv' => $robot['login'], 'winner' => $sim->getWinner()['login']]);
+                    echo json_encode(['data' => $data['selected'], 'animations' => $this->sim->getAnimations() ,'log' => $log, 'adv_cards' => $cartesRobot, 'adv' => $robot['login'], 'winner' => $this->sim->getWinner()['login']]);
                     return true;
                 
                 } 
                 else if ($this->params['get']['action'] == 'send_selection') {
                     $data = json_decode($this->params['post']['data'],true); // decode as array
                     $j1 = $_SESSION['user']; // $j1 = userHydro->hydrate($_SESSION['user'])  
-                    $gameRepository = new \Game\GameRepository($this->connection);
-                    $gameHydrator = new \Game\GameHydrator();
-                    $game = $gameRepository->findGameById($data['game']['id']);
+                    $game = $this->gameRepository->findGameById($data['game']['id']);
                     $cards = $game->getCards();
 
                     if($cards == NULL ){
                         
                         $cardArray = json_encode([$j1['login'] => $data['selected'],$data['adv'] => '']);
 
-                        $gameRepository->updateCards($data['game']['id'],$cardArray);
+                        $this->gameRepository->updateCards($data['game']['id'],$cardArray);
 
                         echo json_encode(['data' => $data['selected'], 'log' => '', 'resolved' => false]);
                     }
                     else{
                         $cardArray = json_decode($cards,true);
-                        $sim = new Simulator();
                         $j2 = ['login'=> $data['adv']];
-                        $winner = $sim->simulate($data['selected'],$cardArray[$data['adv']],$j1,$j2);
-                        foreach($sim->getLog()??[''] as $l){
+                        $winner = $this->sim->simulate($data['selected'],$cardArray[$data['adv']],$j1,$j2);
+                        foreach($this->sim->getLog()??[''] as $l){
                             $log[] = $l;
                         }
                         
@@ -118,70 +118,63 @@ class FrontController
                             $log[] = "Exéco.";
                         }
                         else{
-                            $gameRepository->updateWinner($data['game']['id'],$winner['login']);
+                            $this->gameRepository->updateWinner($data['game']['id'],$winner['login']);
 
                             $log[] = "Le joueur ".$winner['login']." a gagné!";
                         }
-                        $gameRepository->updateMessages($data['game']['id'],json_encode(['log' => $log, 'animations' => $sim->getAnimations()]));
+                        $this->gameRepository->updateMessages($data['game']['id'],json_encode(['log' => $log, 'animations' => $this->sim->getAnimations()]));
                         
-                        $gameRepository->updateCards($data['game']['id'],json_encode($cardsEv));
+                        $this->gameRepository->updateCards($data['game']['id'],json_encode($cardsEv));
 
                         
-                        echo json_encode(['data' => $data['selected'], 'log' => $log, 'animations' => $sim->getAnimations(), 'resolved' => true, 'adv_cards' => $cardArray[$data['adv']], 'winner' => $sim->getWinner()]);
+                        echo json_encode(['data' => $data['selected'], 'log' => $log, 'animations' => $this->sim->getAnimations(), 'resolved' => true, 'adv_cards' => $cardArray[$data['adv']], 'winner' => $this->sim->getWinner()]);
 
                     }
                     return true;
                 } 
                 else if ($this->params['get']['action'] == 'connect') {
-                    $gameRepository = new \Game\GameRepository($this->connection);
-                    $gameHydrator = new \Game\GameHydrator();
                     $j = $_SESSION['user'];
-                    $game = $gameRepository->findPlayer($j);
-                    $c = true;
+                    $game = $this->gameRepository->findPlayer($j);
+                    $connected = true;
                     $adv = '';
                     if(strlen($game->getIdPlayer2().'') == 0){
-                        $c = false;
+                        $connected = false;
                     }
                     else{
-                        $adv = $gameRepository->findOtherLogin($data['id'],$_SESSION['login']);
+                        $adv = $this->gameRepository->findOtherLogin($data['id'],$_SESSION['login']);
                     }
-                    echo json_encode(['game' => $gameHydrator->extract($game), 'log' => '', 'connected' => $c,'adv' => $adv]);
+                    echo json_encode(['game' => $this->gameHydrator->extract($game), 'log' => '', 'connected' => $connected,'adv' => $adv]);
                     return true;
                 
                 } else if ($this->params['get']['action'] == 'ping_server') {
-                    $gameRepository = new \Game\GameRepository($this->connection);
-                    $gameHydrator = new \Game\GameHydrator();
-
                     $data = json_decode($this->params['post']['data'],true);
                     $j = $_SESSION['user'];
                     
-                    $game = $gameRepository->findGameById($data['id']);
+                    $game = $this->gameRepository->findGameById($data['id']);
 
-                    $c = true;
+                    $connected = true;
                     $adv2 = '';
                     if(strlen($game->getIdPlayer2().'') == 0){
-                        $c = false;
+                        $connected = false;
                     }
                     else{
-                        $adv2 = $gameRepository->findOtherLogin($data['id'],$_SESSION['login']);
+                        $adv2 = $this->gameRepository->findOtherLogin($data['id'],$_SESSION['login']);
                     }
-                    echo json_encode(['game' => $gameHydrator->extract($game), 'log' => '', 'connected' => $c, 'adv' => $adv2]);
+                    echo json_encode(['game' => $this->gameHydrator->extract($game), 'log' => '', 'connected' => $connected, 'adv' => $adv2]);
                     return true;
                 }
                 else if ($this->params['get']['action'] == 'ping_resolution') {
-                    $gameRepository = new \Game\GameRepository($this->connection);
-                    $gameHydrator = new \Game\GameHydrator();
 
                     $data = json_decode($this->params['post']['data'],true);
                     
-                    $game = $gameRepository->findGameById($data['game']['id']);
+                    $game = $this->gameRepository->findGameById($data['game']['id']);
                     $log = json_decode($game->getMessages(),true);
-                    $r = true;
+                    $resolved = true;
                     if($log == NULL){
-                        $r = false;
+                        $resolved = false;
                     }
-                    $winner = $gameRepository->findWinnerLogin($data['game']['id']);
-                    echo json_encode(['game' => $gameHydrator->extract($game), 'log' => $log['log'], 'animations' => $log['animations'], 'resolved' => $r, 'winner' => $winner]);
+                    $winner = $this->gameRepository->findWinnerLogin($data['game']['id']);
+                    echo json_encode(['game' => $this->gameHydrator->extract($game), 'log' => $log['log'], 'animations' => $log['animations'], 'resolved' => $resolved, 'winner' => $winner]);
                     return true;
                 }
                 return false;
