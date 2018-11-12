9f1dc25cd6c74e987317a4c01c9e467e4f83bd21
Kévin Bourdouleix <kevin.bourdouleix@gmail.com>
2018-11-12 22:14:54 +0100

    Refix register

--- src/User/UserHydrator.php
+++ src/User/UserHydrator.php
@@ -47,8 +47,8 @@ class UserHydrator
             ->setLogin($data['login'] ?? null)
             ->setPassword($data['password'] ?? null)
             ->setEcts($data['ects'] ?? null)
-            ->setIsAdmin($data['isadmin'] ?? null)
-            ->setIsActif($data['isactif'] ?? null);
+            ->setIsAdmin($data['isAdmin'] ?? null)
+            ->setIsActif($data['isActif'] ?? null);
     }
 
 }
